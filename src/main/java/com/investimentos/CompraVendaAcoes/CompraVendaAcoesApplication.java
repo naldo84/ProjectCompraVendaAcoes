7de2b2c30cd9ea445d28a59ad9f7d2cbb7f99453
Erinaldo <erinaldo2005@gmail.com>
2025-02-03 01:22:56 -0300

    Início - Estrutura do projeto

---
+++ src/main/java/com/investimentos/CompraVendaAcoes/CompraVendaAcoesApplication.java
@@ -0,0 +1,13 @@
+package com.investimentos.CompraVendaAcoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraVendaAcoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraVendaAcoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investimentos/CompraVendaAcoes/CompraVendaAcoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investimentos.CompraVendaAcoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraVendaAcoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
